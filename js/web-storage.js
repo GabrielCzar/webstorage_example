4a10085df1811d3195ec4d5dbbddae31a6a5e2b2
Gabriel Cesar <gcal1@live.com>
2017-01-17 23:45:23 -0300

    armazenando dados com o web storage do HTML 5

---
+++ js/web-storage.js
@@ -0,0 +1,30 @@
+/**
+ * Created by gabriel on 17/01/17.
+ */
+
+function atualizaSaudacao() {
+    var saudacao = document.getElementById("saudacao");
+
+    if (localStorage.nome) {
+        saudacao.innerHTML = "Olá " + localStorage.nome;
+    } else {
+        saudacao.innerHTML = null;
+    }
+};
+
+window.onload = function () {
+    var btnEnviar = document.getElementById("btnEnviar");
+    btnEnviar.onclick = function () {
+        var campoNome = document.getElementById("campoNome");
+        localStorage.nome = campoNome.value;
+        atualizaSaudacao();
+    };
+
+    var btnLimpar = document.getElementById("btnLimpar");
+    btnLimpar.onclick = function () {
+        localStorage.removeItem("nome");
+        atualizaSaudacao();
+    };
+
+    atualizaSaudacao();
+};
